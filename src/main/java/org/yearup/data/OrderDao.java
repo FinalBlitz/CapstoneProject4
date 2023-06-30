9a4396abdf2d4daebc2350f2e7c4342cf9fb6571
iconbaypark2900 <jonaston015@gmail.com>
2023-06-29 20:09:15 -0400

    go away

---
+++ src/main/java/org/yearup/data/OrderDao.java
@@ -0,0 +1,10 @@
+package org.yearup.data;
+
+//public class OrderDao {
+//    Order createOrder(Order order);
+//    Order getOrderById(int orderId);
+//    List<Order> getAllOrders();
+//    List<Order> getOrdersByUserId(int userId);
+//    void updateOrder(Order order);
+//    void deleteOrder(int orderId);
+//}
